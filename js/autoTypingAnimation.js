98fc062c6cd51e49cf3f3607114fb4d0faefeb89
Kandula Naveen <104118171+Kandulanaveennaidu@users.noreply.github.com>
2024-05-25 12:03:35 +0530

    Updated

--- js/autoTypingAnimation.js
+++ js/autoTypingAnimation.js
@@ -1,7 +1,7 @@
 setTimeout(function () {
   $('.about-info-inner').typed({
     strings: [
-      "<span>Hi, I am Kandula Naveen</span>  a dedicated professional passionate about leveraging technology to create impactful solutions. With a keen eye for detail and a commitment to innovation, I specialize in crafting seamless experiences and robust digital strategies. My goal is to deliver exceptional results while adapting to evolving challenges."
+      "<span>Full-Stack Developer | Building Engaging Web Solutions Results-oriented Full-Stack Developer with a proven track record of designing, developing, and deploying user-centric web applications. Expertise in crafting intuitive front-end experiences with React.js, Redux.js, and Tailwind CSS, coupled with a strong foundation in building scalable backend infrastructure using Node.js, Express, and MySQL. Demonstrated experience in integrating real-time chat functionalities, chatbots, and RESTful APIs to enhance user engagement and streamline data flow. Key Achievements: Enhanced user engagement on ATTPL's Election Management Service (EMS) platform by integrating real-time chat and chatbots. Developed and deployed a responsive website for Teachoptima, contributing to an improved online presence. Collaborated effectively with cross-functional teams to deliver high-quality solutions that meet business objectives. Seeking a challenging Full-Stack Developer role to leverage technical expertise and contribute to innovative projects in a fast-paced environment."
     ],
     typeSpeed: 7,
     contentType: 'html'
